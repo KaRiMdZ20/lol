ae921a7e638574353473a802d4d01b9993aae5b8
KaRiMdZ20 <57139937+KaRiMdZ20@users.noreply.github.com>
2019-10-29 17:53:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+const { Client, Util } = require('discord.js');  
+const getYoutubeID = require('get-youtube-id'); 
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const request = require('request');
 const queue = new Map(); 
 const client = new Discord.Client(); 
 const db = require('quick.db');
 const giphy = require('giphy-api')();    
+const googl = require('goo.gl'); 
+const translate = require('google-translate-api');   
 const fs = require("fs"); 
 const moment = require("moment");
 const UserBlocked = new Set(); 
@@ -19,52 +26,6 @@ const dateFormat = require('dateformat');
 const pretty = require('pretty-ms') 
 var Canvas = require('canvas')
 var prefix = "!";
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-
-client.on('ready', function(){
-    var ms = 15000 ;
-    var setGame = [``,'',``,'',''];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i]);
-    }, ms);
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-
-
-
 
 client.on('message', message => {
 
