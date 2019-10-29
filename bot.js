e9db22f331b7135052a54aff7df0b88f3fb9946f
KaRiMdZ20 <57139937+KaRiMdZ20@users.noreply.github.com>
2019-10-29 17:42:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,23 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 const { Client, Util } = require('discord.js');  
 const request = require('request');
 const queue = new Map(); 
 const client = new Discord.Client(); 
+const db = require('quick.db');
+const giphy = require('giphy-api')();    
 const fs = require("fs"); 
 const moment = require("moment");
 const UserBlocked = new Set(); 
 const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const zalgo = require('zalgolize');   
+const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
+const pretty = require('pretty-ms') 
 var Canvas = require('canvas')
 
 
